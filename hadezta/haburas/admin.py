a8e3a1e18fdcd0aab24dc32bba50747055c022c5
Mariano de Deus <dedeus_mariano@yahoo.com>
2018-08-27 17:09:36 +0900

    add registu kursu to admin

--- hadezta/haburas/admin.py
+++ hadezta/haburas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RegistuKursu
+class RegistuKursuAdmin(admin.ModelAdmin):
+    model = RegistuKursu
+    list_display = ('first_name', 'last_name', 'birthday', 'place', 'phonenumber', 'sexo', 'cursu')
+    search_fields = ('first_name', 'last_name', 'place', 'phonenumber')
+
+
+admin.site.register(RegistuKursu, RegistuKursuAdmin)
