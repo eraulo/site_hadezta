304035f156c2ee2f3f28cb756718567acfd97937
Mariano de Deus <dedeus_mariano@yahoo.com>
2018-08-29 10:33:40 +0900

    change site name in the admin

--- hadezta/haburas/admin.py
+++ hadezta/haburas/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
 from .models import RegistuKursu
+
+
 class RegistuKursuAdmin(admin.ModelAdmin):
     model = RegistuKursu
     list_display = ('first_name', 'last_name', 'birthday', 'place', 'phonenumber', 'sexo', 'cursu')
@@ -9,3 +12,6 @@ class RegistuKursuAdmin(admin.ModelAdmin):
 
 
 admin.site.register(RegistuKursu, RegistuKursuAdmin)
+
+# Admin site header name
+admin.site.site_header = "%s %s" % ("Hadezta", _("administration"))
